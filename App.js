8b6074670a5087131e886dab9da4722e4a8f535f
Salah Eddine Bachir-Cherif <gs_bachircherif@esi.dz>
2018-09-13 22:53:28 +0100

    Removed duplicates

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
-
 import SwipeCards from 'react-native-swipe-cards';
-import { SwitchNavigator } from 'react-navigation';
-import { Loading, Login, Main, SignUp } from './src/components';
+import {SwitchNavigator} from 'react-navigation';
+import {Loading,Login,Main,SignUp} from "./src/components";
+
+import {  ScrollView } from 'react-native';
+
 import { DrawerNavigator } from 'react-navigation';
 import MainDrawer from './src/components/drawer';
 const App = DrawerNavigator(
